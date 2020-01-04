b99ed23b78ed8be2bcc28522edb7e03129696643
zachgoll <zach.gollwitzer@gmail.com>
2020-01-04 09:34:38 -0500

    Add routing to login and register page

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   @ViewChild('registerform', { static: false }) registerForm: NgForm;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
@@ -43,6 +44,7 @@ export class RegisterComponent implements OnInit {
       // When observable completes
       () => {
         console.log('done!');
+        this.router.navigate(['login']);
       }
 
     );
